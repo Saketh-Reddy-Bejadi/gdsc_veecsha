235b67b06fed0e182eb70790d61412dcb6c42acf
Saketh Reddy Bejadi <bejadisakethreddy168@gmail.com>
2024-02-21 01:56:17 +0530

    added transition for sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,23 +1,33 @@
 import { Link } from "react-router-dom";
-import "./SideBar.css"
+import "./SideBar.css";
 
-export const SideBar = ({viewBox,toggleMenu}) => {
-
-  const ClickedBtn=()=>{
+export const SideBar = ({ viewBox, toggleMenu }) => {
+  const ClickedBtn = () => {
     toggleMenu();
-  }
-  
+  };
+
   return (
     <>
-        <div id='sidebarHide' className={viewBox ? "sidebar" : "disnone"}>
-            <p>Menu</p>
-            <div className="SBcontents" >
-              <Link onClick={ClickedBtn} to="/" >Home</Link>
-              <Link onClick={ClickedBtn} to="/Services" >Services</Link>
-              <Link onClick={ClickedBtn} to="/WebDev">Web Dev</Link>
-              <Link onClick={ClickedBtn} to="/DSA">DSA</Link>
-            </div>
+      <div
+        id="sidebarHide"
+        className={`sidebar ${viewBox ? "open" : "closed"}`}
+      >
+        <p>Menu</p>
+        <div className="SBcontents">
+          <Link onClick={ClickedBtn} to="/">
+            Home
+          </Link>
+          <Link onClick={ClickedBtn} to="/Services">
+            Services
+          </Link>
+          <Link onClick={ClickedBtn} to="/WebDev">
+            Web Dev
+          </Link>
+          <Link onClick={ClickedBtn} to="/DSA">
+            DSA
+          </Link>
         </div>
-    </>   
-  )
-}
+      </div>
+    </>
+  );
+};
