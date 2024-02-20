644a2da1303713e8afffa025ad2f7e5447f2ecd0
Saketh Reddy Bejadi <bejadisakethreddy168@gmail.com>
2024-02-20 23:59:30 +0530

    done with web dev page

--- src/components/WebDev/WebDev.js
+++ src/components/WebDev/WebDev.js
@@ -5,12 +5,13 @@ import js from "../../assets/js.svg";
 import reactJS from "../../assets/reactJS.svg";
 import nodeJS from "../../assets/nodejs.svg";
 import { useEffect } from "react";
-import data from "./data";
+import data, { webDev } from "./data";
 
 export const WebDev = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
   return (
     <div className="WebDev">
       <div className="WebCon1">
@@ -31,57 +32,42 @@ export const WebDev = () => {
           functionality and building server-side logic.
         </p>
         <ul className="wBasic">
-          <li>
-            <span className="webSidehead">Front-end development:</span> Focuses
-            on the client-side, what users see and interact with directly user
-            interface (UI) and user experience (UX).
-            <br />
-            <span>Key technologies include:</span>
-          </li>
-          <li>
-            <span className="webSidehead">Back-end development:</span> deals
-            with the server-side, the behind-the-scenes logic that powers
-            websites. Common technologies include:
-            <br />
-            <span>Key technologies include:</span>
-          </li>
-          <li>
-            <span className="webSidehead">Full-stack development:</span>{" "}
-            Combining both front-end and back-end development skills.
-          </li>
+          {webDev && webDev.length > 0
+            ? webDev.map((t, k) => (
+                <div key={k}>
+                  {t.cate.map((category, m) => (
+                    <li key={m}>
+                      <span className="webSidehead">{category.head}</span>
+                      {category.content} <br />
+                      <span>{category.sidetech}</span>
+                      {category.techs?        <ul className="subItem">
+                        {category.techs.map((tech, n) => (
+                          <li key={n}>
+                            <span className="subIsh">
+                              <a href="/">{tech.subhead}</a>
+                            </span>
+                            {tech.explaination}
+                          </li>
+                        ))}
+                      </ul>:null}
+                    </li>
+                  ))}
+                </div>
+              ))
+            : null}
+
         </ul>
-        <p className="er4rsg">
-          Web development offers numerous benefits, including: <br />
-        </p>
-        <ul className="wBasic">
-          <li>
-            <span className="webSidehead">Creative expression:</span> Bring
-            ideas to life by building interactive and visually appealing web
-            experiences. Problem-solving: Devise solutions to technical
-            challenges and user needs through code.
-          </li>
-          <li>
-            <span className="webSidehead">Demand and growth:</span> The field is
-            in high demand with ample job opportunities and career growth
-            potential.{" "}
-          </li>
-          <li>
-            <span className="webSidehead">Impact:</span> Create websites and
-            applications that can influence and inform people worldwide. Remote
-            work possibilities: Many web development roles allow for remote
-            work, offering flexibility and location independence.
-          </li>
-        </ul>
+        
       </div>
       <div className="roadmap">
         {data && data.length > 0
-          ? data.map((dataItem) => (
-              <>
+          ? data.map((dataItem, j) => (
+              <div key={j}>
                 <a className="item" href="/WebDev">
                   <p className="Itext">{dataItem.heading}</p>
                 </a>
                 {dataItem.id < 5 ? <div className="path"></div> : null}
-              </>
+              </div>
             ))
           : null}
       </div>
--- src/components/WebDev/data.js
+++ src/components/WebDev/data.js
@@ -21,3 +21,64 @@ const data = [
   },
 ];
 export default data;
+
+
+const webDev = [
+  {
+    type: "web",
+    cate: [
+      {
+        head: "Front-end development:",
+        content:
+          "Focuses on the client-side, what users see and interact with directly user interface (UI) and user experience (UX).",
+        sidetech: "Key technologies include:",
+        techs: [
+          {
+            subhead: "HTML",
+            explaination:
+              ": Hypertext Markup Language, the foundation for structuring web pages and defining content and the website's foundation.",
+          },
+          {
+            subhead: "CSS",
+            explaination:
+              ": Cascading Style Sheets, controls a website's visual appearance, layout, and styling. Also using frameworks like Bootstrap or Tailwind CSS.",
+          },
+          {
+            subhead: "JavaScript",
+            explaination:
+              ": Adds dynamic behavior and interactivity to web pages, enabling features like animations and form validation and Adds interactivity with libraries like React, Angular, or Vue.js.",
+          },
+        ],
+      },
+      {
+        head: "Back-end development:",
+        content:
+          "Focuses on the server-side, handling requests from the client-side and interacting with databases.",
+        sidetech: "Key technologies include:",
+        techs: [
+          {
+            subhead: "Node.js",
+            explaination:
+              ": A JavaScript runtime environment that allows you to run JavaScript on the server-side, enabling the development of scalable network applications.",
+          },
+          {
+            subhead: "Express.js",
+            explaination:
+              ": A web application framework for Node.js, used for building APIs and web applications.",
+          },
+          {
+            subhead: "Databases (SQL or NoSQL)",
+            explaination:
+              ": Storage solutions for web applications, handling data storage and retrieval. Examples include MySQL, MongoDB, PostgreSQL, etc.",
+          },
+        ],
+      },
+      {
+        head:'Full-stack development',
+        content:" building both the front-end (what users see and interact with) and the back-end (the 'behind-the-scenes' workings) of a website or web application. It's like being a chef who can both cook and decorate a cake. Full stack developers know how to handle everything from designing the look of a website to making sure it works smoothly and securely behind the scenes."
+      }
+    ],
+  },
+];
+
+export { webDev };
